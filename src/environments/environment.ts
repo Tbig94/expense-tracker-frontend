6b31eb0175b6104546d05c2dc4fcd36f69482470
Tamas Nagy <nagy411@gmail.com>
2026-08-18 22:59:53 +0200

    small fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://localhost:7054/api',
+  //apiUrl: 'https://localhost:7054/api',
   bearer: '',
   apiUrl: 'https://expense-tracker-backend-dnb7ada2bdare0fz.swedencentral-01.azurewebsites.net/api',
 };
