ee88305dacaf5f6cf7e10fc47db453ffcc740061
Tamas Nagy <nagy411@gmail.com>
2026-08-20 11:57:11 +0200

    category card fix, nav fix

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -6,7 +6,6 @@ import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
