89a7bbd07ad0cca15c4473cfa229efbddda45c41
Tamas Nagy <nagy411@gmail.com>
2026-08-29 19:23:02 +0200

    statistics page tab group

--- src/app/features/statistics/statistics.ts
+++ src/app/features/statistics/statistics.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { MonthlyStatistics } from './monthly-statistics/monthly-statistics';
 import { YearlyStatistics } from './yearly-statistics/yearly-statistics';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-statistics',
-  imports: [MonthlyStatistics, YearlyStatistics, MatButtonModule],
+  imports: [MonthlyStatistics, YearlyStatistics, MatButtonModule, MatTabsModule],
   templateUrl: './statistics.html',
   styleUrl: './statistics.css',
 })
